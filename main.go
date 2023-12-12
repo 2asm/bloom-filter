6c52262c2b0755e898e0f3ae5f27c44069f8cf06
2asm <0077dk0077@gmail.com>
2023-12-12 11:10:15 +0530

    changed package

---
+++ main.go
@@ -0,0 +1,16 @@
+package main 
+
+import (
+	"fmt"
+
+	"github.com/2asm/bloom-filter/bloomfilter"
+)
+func main() {
+	// error_rate, number of insertions
+	bf := bloomfilter.NewBloomFilter(0.01, 1000000)
+	bf.Add("hi")
+	bf.Add("hello there")
+	fmt.Printf("%v\n", bf.Contains("hi"))
+	fmt.Printf("%v\n", bf.Contains("hello there"))
+	fmt.Printf("%v\n", bf.Contains("we"))
+}
