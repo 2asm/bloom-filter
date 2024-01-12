0ca76bdb351a70f2b4eb457cb72c47d9e63f1875
2asm <0077dk0077@gmail.com>
2024-01-12 22:52:15 +0530

    combinatorial hash generation

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
-package main 
+package main
 
 import (
 	"fmt"
 
 	"github.com/2asm/bloom-filter/bloomfilter"
 )
+
 func main() {
 	// error_rate, number of insertions
 	bf := bloomfilter.NewBloomFilter(0.001, 100000)
